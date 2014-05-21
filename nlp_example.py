dfc9aa01467820f07dc7eef366cf3266d5cad681
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-05-21 18:53:52 +0200

    Compress a but more the code

--- nlp_example.py
+++ nlp_example.py
@@ -11,12 +11,13 @@ clf = pipeline.Pipeline([('step1', feature_extraction.text.TfidfVectorizer(min_d
 clf.fit(data.data, data.target)
 
 # Demo
-clf.predict_proba(["This movie is the worst I ever saw."])[0, 0]  # negative probability
+clf.predict_proba(["Worst movie I ever saw"])[0, 0]  # negative probability
 # 0.99929299972040175
-clf.predict_proba(["Shawshank Redemption, eat your heart out!"])[0, 1]  # positive probability
-# 0.83124224591605844
+clf.predict_proba(["Godzilla, eat your heart out!"])[0, 1]  # positive probability
+# 0.64533559029803611
+print clf.predict(["This movie is the worst I ever saw.",
+"Shawshank Redemption, eat your heart out!"])
 
 # Measure the prediction score by cross validation
 from sklearn import cross_validation
-print cross_validation.cross_val_score(clf, data.data, data.target, cv=5,
-verbose=10)
+print cross_validation.cross_val_score(clf, data.data, data.target)

1c93e8f25507c280442652892ed5c4fdda26becb
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-11-07 08:43:56 +0100

    Integrate on-line learning example

--- online_learning_example.py
+++ online_learning_example.py
@@ -8,7 +8,7 @@ X_test = vectorizer.fit_transform(dataset.data[:1000])
 y_test = dataset.target[:1000]
 
 from sklearn.linear_model import SGDClassifier
-classifier = SGDClassifier()     # SVM classifier that learns online using SGD.
+classifier = SGDClassifier()    # SVM classifier trained online with stochastic gradient descent.
 
 # Artificially make long train data
 train_data = 10 * dataset.data[1000:]
@@ -17,6 +17,6 @@ train_target = 10 * list(dataset.target[1000:])
 for i in range(0, len(train_data), 1000):    # Iterate on mini-batchs of 1000 samples
     y_train = train_target[i:i + 1000]
     X_train = vectorizer.fit_transform(train_data[i:i + 1000])
-    # update estimator with examples in the current mini-batch
+    # update the classifier with documents in the current mini-batch
     classifier.partial_fit(X_train, y_train, classes=range(len(dataset.target_names)))
     print classifier.score(X_test, y_test)

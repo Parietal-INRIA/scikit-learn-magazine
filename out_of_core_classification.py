b433f78c0ddfa1aa32b0b915953a61fa55e5dac4
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-11-07 08:37:49 +0100

    Integrate online learning example

--- out_of_core_classification.py
+++ out_of_core_classification.py
@@ -1,30 +1,22 @@
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import HashingVectorizer
 
-# Create the hasher
-hasher = HashingVectorizer(decode_error='ignore', n_features=2 ** 16)
+vectorizer = HashingVectorizer(decode_error='ignore', n_features=2 ** 16) # A vectorizer
 
-dataset = fetch_20newsgroups(shuffle=True, random_state=1,
-                             remove=('headers', 'footers', 'quotes'))
-
-X_test = hasher.fit_transform(dataset.data[:1000])
+dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers'))
+X_test = vectorizer.fit_transform(dataset.data[:1000])
 y_test = dataset.target[:1000]
 
 from sklearn.linear_model import SGDClassifier
-# SVM classifier that learns online using SGD.
-classifier = SGDClassifier()
+classifier = SGDClassifier()     # SVM classifier that learns online using SGD.
 
 # Artificially make long train data
-train_data = 15 * dataset.data[1000:]
-train_target = 15 * list(dataset.target[1000:])
+train_data = 10 * dataset.data[1000:]
+train_target = 10 * list(dataset.target[1000:])
 
-# Main loop : iterate on mini-batchs of 1000 samples
-i = 0
-while i < len(train_data) - 1000:
+for i in range(0, len(train_data), 1000):    # Iterate on mini-batchs of 1000 samples
     y_train = train_target[i:i + 1000]
-    X_train = hasher.fit_transform(train_data[i:i + 1000])
+    X_train = vectorizer.fit_transform(train_data[i:i + 1000])
     # update estimator with examples in the current mini-batch
-    classifier.partial_fit(X_train, y_train,
-                           classes=range(len(dataset.target_names)))
+    classifier.partial_fit(X_train, y_train, classes=range(len(dataset.target_names)))
     print classifier.score(X_test, y_test)
-    i += 1000

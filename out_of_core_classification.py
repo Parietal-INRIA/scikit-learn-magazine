4f6db974d0b1a6d6a1853978dd8afb2eeff83d7f
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-11-07 07:25:25 +0100

    Somewhat happy with the on-line learning example

--- out_of_core_classification.py
+++ out_of_core_classification.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import HashingVectorizer
 
@@ -12,21 +10,21 @@ dataset = fetch_20newsgroups(shuffle=True, random_state=1,
 X_test = hasher.fit_transform(dataset.data[:1000])
 y_test = dataset.target[:1000]
 
-from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.linear_model import SGDClassifier
 # SVM classifier that learns online using SGD.
-classifier = PassiveAggressiveClassifier(C=.1)
+classifier = SGDClassifier()
 
-#from sklearn.linear_model import SGDClassifier
-## SVM classifier that learns online using SGD.
-#classifier = SGDClassifier(learning_rate='invscaling', eta0=1.)
+# Artificially make long train data
+train_data = 15 * dataset.data[1000:]
+train_target = 15 * list(dataset.target[1000:])
 
 # Main loop : iterate on mini-batchs of 1000 samples
-i = 1
-while i * 1000 < len(dataset.data) - 2000:
-    y_train = dataset.target[i * 1000:(i + 1) * 1000]
-    X_train = hasher.fit_transform(dataset.data[i * 1000:(i + 1) * 1000])
+i = 0
+while i < len(train_data) - 1000:
+    y_train = train_target[i:i + 1000]
+    X_train = hasher.fit_transform(train_data[i:i + 1000])
     # update estimator with examples in the current mini-batch
     classifier.partial_fit(X_train, y_train,
                            classes=range(len(dataset.target_names)))
-    print(classifier.score(X_test, y_test))
-    i += 1
+    print classifier.score(X_test, y_test)
+    i += 1000
